0ce9709fee256646011919422ccca25df74b9e47
Lubba-64 <Lubba64@gmail.com>
2025-12-07 11:33:05 -0500

    fix: Fix script being named wrong

--- server_control.sh
+++ server_control.sh
@@ -2,16 +2,16 @@
 case "$1" in
     start)
         echo "Starting server..."
-        sudo systemctl enable --now multiplayer-tierlist
+        sudo systemctl enable --now template
         ;;
     stop)
-    sudo systemctl stop multiplayer-tierlist   # Replace with your service name
+    sudo systemctl stop template   # Replace with your service name
 
         ;;
     restart)
     sudo systemctl daemon-reload
-    sudo systemctl restart multiplayer-tierlist
-    sudo systemctl status multiplayer-tierlist
+    sudo systemctl restart template
+    sudo systemctl status template
         ;;
     *)
         echo "Usage: $0 {start|stop|restart}"
